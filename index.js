225e3338172b0b6e69547b93d94ea4728ce7be4e
Budi Waskita <etherglow@gmail.com>
2021-02-11 21:23:45 +0700

    Try Winston Logger

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
-const express = require('express')
-const app = express()
+
 const port = process.env.PORT || 3000
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json())
+const app = express()
+const winston = require('winston')
+const consoleTransport = new winston.transports.Console()
+const myWinstonOptions = {
+    transports: [consoleTransport]
+}
+const logger = new winston.createLogger(myWinstonOptions)
 
-app.use(express.json())
-var LoremIpsum = require('lorem-ipsum').LoremIpsum;
+function logRequest(req, res, next) {
+    logger.info(req.url)
+    next()
+}
+app.use(logRequest)
 
-var lorem = new LoremIpsum({
-  sentencesPerParagraph: {
-    max: 8,
-    min: 4
-  },
-  wordsPerSentence: {
-    max: 16,
-    min: 4
-  }
-});
-
-app.get('/', (req, res) => res.send(lorem.generateParagraphs(7)))
-app.post('/update', (req, res) => {
-  console.log('body is ',req.body);
-  res.send(req.body);
-})
-
+function logError(err, req, res, next) {
+    logger.error(err)
+    next()
+}
+app.use(logError)
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
